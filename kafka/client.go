c817af5b9f6d5e702ce5afe4fab60bb3423fb713
Rahul Khairwar <rahulkhairwar@gmail.com>
2021-01-19 19:16:48 +0800

    Updated Kafka

--- kafka/client.go
+++ kafka/client.go
@@ -1,5 +1,7 @@
 package kafka
 
+import "fmt"
+
 type Client struct {
 	Id string `json:"id"`
 	Field1 int64 `json:"field1"`
@@ -7,4 +9,8 @@ type Client struct {
 
 func (c *Client) DoSomething(s string) bool {
 	return s == ""
+}
+
+func Test() {
+	fmt.Println("test...")
 }
