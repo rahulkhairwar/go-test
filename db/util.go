3b6d0da77c9a28cb0401a8a67125c5c574645f78
Rahul Khairwar <rahulkhairwar@gmail.com>
2021-02-15 15:59:54 +0800

    Added db.SomeFuncForV0_0_2()

--- db/util.go
+++ db/util.go
@@ -1,7 +1,14 @@
 package db
 
-import "github.com/rahulkhairwar/go-test/sql"
+import (
+	"fmt"
+	"github.com/rahulkhairwar/go-test/sql"
+)
 
 func TestImportSql() {
 	sql.TestForSQLV0_0_3()
+}
+
+func SomeFuncForV0_0_2() {
+	fmt.Println("db > util.go > SomeFuncForV0_0_2()")
 }
