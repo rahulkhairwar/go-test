345e52ff517e371ded397f25534f38a859bb1115
Rahul Khairwar <rahulkhairwar@gmail.com>
2021-02-15 16:01:58 +0800

    Added sql.TestForSQLV0_0_4() and deleted sql.Test2()

--- sql/client.go
+++ sql/client.go
@@ -6,9 +6,9 @@ func Test() {
 	fmt.Println("test...")
 }
 
-func Test2() {
-	fmt.Println("test2...")
-}
+// func Test2() {
+// 	fmt.Println("test2...")
+// }
 
 func TestForV6() {
 	fmt.Println("test v0.0.6")
@@ -16,4 +16,8 @@ func TestForV6() {
 
 func TestForSQLV0_0_3() {
 	fmt.Println("test sql v0.0.3")
+}
+
+func TestForSQLV0_0_4() {
+	fmt.Println("test sql v0.0.4")
 }
