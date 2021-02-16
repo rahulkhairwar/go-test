b630fd1db383e98c2c3575a990eec7b663f91bb1
Rahul Khairwar <rahulkhairwar@gmail.com>
2021-02-16 15:03:42 +0800

    Changes to noCompile.go

--- noCompile.go
+++ noCompile.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 func init() {
 	panic("not allowed to import common by itself, import individual packages")
+}
+
+func main() {
+	fmt.Println("common > noCompile.go > main(). This should panic")
 }
 
 // type noCompile struct
